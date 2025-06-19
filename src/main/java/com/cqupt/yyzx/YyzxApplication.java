affa5005c89ca1cb02d1cdb8c8d0a97df6729a12
kong-long-bye <2968498506@qq.com>
2025-06-19 08:49:12 +0800

    初始画项目

---
+++ src/main/java/com/cqupt/yyzx/YyzxApplication.java
@@ -0,0 +1,13 @@
+package com.cqupt.yyzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyzxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyzxApplication.class, args);
+    }
+
+}
